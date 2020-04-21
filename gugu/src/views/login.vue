22fc01b12bbcfb025afdab3d49aae1601ae05260
PC-20191203BFBA\Administrator <>
2020-04-21 14:13:08 +0800

    login 1.0 2020.04.21

--- gugu/src/views/login.vue
+++ gugu/src/views/login.vue
@@ -16,47 +16,72 @@
 			hoverMode="grab"
 			:clickEffect="true"
 			clickMode="push"
-      class="particles"
+			class="particles"
 		>
 		</vue-particles>
-    <!-- 登录框 -->
-    <div class="login_box">
-      <dv-border-box-7 >
-        <div class="title_2">咕咕</div>
-      </dv-border-box-7>
-    </div>
+		<!-- 登录框 -->
+		<div class="login_box">
+			<dv-border-box-7>
+				<div class="title_2">咕咕</div>
+				<el-form style="padding:0 10%;" class="login">
+					<el-form-item label="账号：">
+						<el-input size="small"
+							v-model="input"
+							placeholder=""
+						></el-input>
+					</el-form-item>
+          <el-form-item label="密码：">
+						<el-input size="small"
+							v-model="input"
+							placeholder=""
+						></el-input>
+					</el-form-item>
+				</el-form>
+        <dv-decoration-9 :color="['rgba(233,233,216,0.8)', 'rgba(233,233,216,0.8)']" class="login_button" style="width:150px;height:80px;color:#fff;font-weight: 600;margin:20% auto">登录</dv-decoration-9>
+			</dv-border-box-7>
+		</div>
 	</dv-full-screen-container>
 </template>
 <script>
 	export default {
 		data() {
 			return {
-
+        input:''
 			}
 		},
 	}
 </script>
-<style lang="scss">
-.login_box{
-  width: 36%;
-  height: 25%;
-  position:fixed;
-  top: 50%;
-  left: 50%;
-  margin: -8% -18%;
-}
-.full_screen_container{
-  background: url('../../public/image_8523.jpg') no-repeat;
-  background-size:100%;
-}
-.particles{
-  width: 100%;
-  height: 100%;
-}
-.title_2{
-  text-align: center;
-  font-size: 32px;
-  line-height: 60px;
-  font-weight: 600;
-}
+<style lang="scss" scoped>
+	.login_box {
+		width: 36%;
+		height: 28%;
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		margin: -8% -18%;
+	}
+	.full_screen_container {
+		background: url('../../public/image_8523.jpg') no-repeat;
+		background-size: 100%;
+	}
+	.particles {
+		width: 100%;
+		height: 100%;
+	}
+	.title_2 {
+		text-align: center;
+		font-size: 32px;
+		line-height: 60px;
+		font-weight: 600;
+	}
+  .login /deep/ .el-input {
+    width:80%;
+    .el-input__inner{
+      background: Transparent;
+      border: 1px solid #5a5a5a;
+    }
+  }
+  .login_button{
+
+  }
 </style>
